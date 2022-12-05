ffd845110ace2d4483b2e60d6edb288f0654cb54
Richard <richard.mrvar@gmail.com>
2022-12-05 09:09:26 +0100

    Completed Day 05.

--- Day05/Day05Parser.py
+++ Day05/Day05Parser.py
@@ -4,4 +4,28 @@ from Shared.Parser import Parser
 
 class Day05Parser(Parser):
     def get_solver(self, text: str) -> Day05Solver:
-        return Day05Solver()
+        index = text.find('move')
+        text1 = text[:index]
+        text2 = text[index:]
+
+        lines1 = text1.rstrip().split('\n')
+        lines1.reverse()
+
+        column_indices = [i for i, c in enumerate(lines1[0]) if c.isnumeric()]
+        columns = [[] for _ in range(len(column_indices))]
+        lines1 = lines1[1:]
+
+        for line in lines1:
+            filled_columns = [(i, c) for i, c in enumerate(line) if c.isalpha()]
+            for i, c in filled_columns:
+                column_index = column_indices.index(i)
+                columns[column_index].append(c)
+
+        text2 = text2.replace('move', '').replace('from', '').replace('to', '')
+
+        instructions = []
+
+        for line in text2.strip().split('\n'):
+            instructions.append(tuple(int(word) for word in line.strip().split()))
+
+        return Day05Solver(columns, instructions)
--- Day05/Day05Solver.py
+++ Day05/Day05Solver.py
@@ -2,8 +2,24 @@ from Shared.Solver import Solver, Part
 
 
 class Day05Solver(Solver):
-    def __init__(self):
-        pass
+    def __init__(self, columns, instructions):
+        self.__columns = columns
+        self.__instructions = instructions
 
     def solve(self, part: Part) -> str:
-        return 'Solver is not implemented!'
+        for instruction in self.__instructions:
+            amount = instruction[0]
+            from_index = instruction[1] - 1
+            to_index = instruction[2] - 1
+
+            if part == Part.A:
+                for i in range(amount):
+                    cargo = self.__columns[from_index].pop()
+                    self.__columns[to_index].append(cargo)
+            else:
+                from_column = self.__columns[from_index]
+                to_column = self.__columns[to_index]
+                self.__columns[from_index] = from_column[:-amount]
+                self.__columns[to_index] = [*to_column, *from_column[-amount:]]
+
+        return ''.join(column[-1] for column in self.__columns)
